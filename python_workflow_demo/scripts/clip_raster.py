9aa758aabea672bbbfb22b8741a101d5f37e5401
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-03-20 14:57:14 +0100

    add editable installs

--- python_workflow_demo/scripts/clip_raster.py
+++ python_workflow_demo/scripts/clip_raster.py
@@ -5,3 +5,10 @@ from project import Project
 p = Project(".")
 
 p.input.dem
+
+# %%
+
+import hhnk_research_tools.general_functions as gf
+
+
+
---
+++ python_workflow_demo/scripts/editable_installs.py
@@ -0,0 +1,4 @@
+# %%
+import hhnk_research_tools.general_functions as gf
+
+gf.test_func()
