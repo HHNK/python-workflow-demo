81040ccf710931dce25e017cd51dc09d7c8cdf41
wietsevangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-04-04 18:27:41 +0200

    add docstrings

--- bin/upload_pypi.bat
+++ bin/upload_pypi.bat
@@ -1,1 +1,2 @@
-"C:\Users\wvangerwen\AppData\Roaming\Python\Python39\Scripts\twine.exe" upload --repository hhnk-research-tools  dist/*
+REM example publishing the package to PyPI
+"C:\Users\wvangerwen\AppData\Roaming\Python\Python39\Scripts\twine.exe" upload --repository python-workflow-demo  dist/*
--- vs-code.cmd
+++ vs-code.cmd
@@ -1,4 +1,5 @@
 : : Opens vs-code in current directory
+: : checks if vs-code insiders is installed when vs-code cannot be found.
 where code >nul 2>nul
 if %errorlevel% neq 0 (
     pixi run code_insiders . | exit
