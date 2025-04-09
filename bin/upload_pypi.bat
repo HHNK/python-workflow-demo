d82336a05e10bdf2950bc3b8826a4d4447423b7c
wietsevangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-04-09 09:00:07 +0200

    add pytest cov

--- bin/upload_pypi.bat
+++ bin/upload_pypi.bat
@@ -1,2 +1,2 @@
 REM example publishing the package to PyPI
-"C:\Users\wvangerwen\AppData\Roaming\Python\Python39\Scripts\twine.exe" upload --repository python-workflow-demo  dist/*
+pixi run twine upload --repository python-workflow-demo  dist/*
